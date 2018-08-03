36827574462af9f3b5cbffc4a32e596c1ca4cbf2
Edison Lim <edison.limjh@gmail.com>
2018-08-04 00:58:38 +0800

    update license to gpl-3.0

--- components/wallet/wallet.js
+++ components/wallet/wallet.js
@@ -1,3 +1,21 @@
+/**
+ This file is part of testrpc.
+  Copyright (c) 2018 - present Zilliqa Research Pvt. Ltd.
+  
+  testrpc is free software: you can redistribute it and/or modify it under the
+  terms of the GNU General Public License as published by the Free Software
+  Foundation, either version 3 of the License, or (at your option) any later
+  version.
+ 
+  testrpc is distributed in the hope that it will be useful, but WITHOUT ANY
+  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+  A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ 
+  You should have received a copy of the GNU General Public License along with
+  testrpc.  If not, see <http://www.gnu.org/licenses/>.
+**/
+
+
 /* Wallet Component */
 const crypto = require('crypto');
 const assert = require('assert');
@@ -7,6 +25,7 @@ var debug_wallet = require('debug')('testrpc:wallet');
 
 
 //@dev: As this is a testrpc, private keys will be stored
+// note: Real systems do not store private key
 
 // Wallet will store three things - address, private key and balance
 wallets = {};
