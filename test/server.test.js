b3ac714cf81593aad17a7a5107b218b14dfe2c95
Edison <edison.limjh@gmail.com>
2018-08-13 17:36:19 +0800

    travis

--- test/server.test.js
+++ test/server.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../app')
-describe('Test the root path', () => {
-    test('It should response the GET method', (done) => {
+describe('Test the Server Connection', () => {
+    test('It should respond to the GET method', (done) => {
         request(app).get('/').then((response) => {
             expect(response.statusCode).toBe(200);
             done();
