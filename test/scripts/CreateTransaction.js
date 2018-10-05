491938e9c6cd6a67b00c01a737cb3f5afbc4dedc
Edison Lim <edison.limjh@gmail.com>
2018-10-06 00:06:50 +0800

    Changed test scripts - GetState fixed (use --contract to specify address) - CreateTransaction and DeployContract now has automatic nonce increment

--- test/scripts/CreateTransaction.js
+++ test/scripts/CreateTransaction.js
@@ -15,94 +15,100 @@
   kaya.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-require('isomorphic-fetch');
-const BN = require('bn.js');
-const { Zilliqa } = require('zilliqa-js');
-
-const url = 'http://localhost:4200';
-const { argv } = require('yargs');
+require("isomorphic-fetch");
+const BN = require("bn.js");
+const { Zilliqa } = require("zilliqa-js");
+const { promisify } = require("util");
+const url = "http://localhost:4200";
+const { argv } = require("yargs");
 
 const zilliqa = new Zilliqa({
   nodeUrl: url,
 });
+
+const makeTxnDetails = (nonceVal) => {
+    txnDetails = {
+        version: 0,
+        nonce: nonceVal,
+        to: recipient,
+        amount: new BN(0),
+        gasPrice: 1,
+        gasLimit: 2000,
+        data: JSON.stringify(msg).replace(/\\"/g, '"'),
+    };
+    return txnDetails;
+};
+
+const getNonceAsync = addr => {
+  return new Promise((resolve, reject) => {
+    zilliqa.node.getBalance({ address: addr }, (err, data) => {
+      if (err || data.error) {
+        reject(err);
+      } else {
+        resolve(data.result.nonce);
+      }
+    });
+  });
+};
 
 let privateKey;
 let recipient;
 
 if (argv.test) {
   // test mode uses keys from the account fixtures
-  privateKey = 'db11cfa086b92497c8ed5a4cc6edb3a5bfe3a640c43ffb9fc6aa0873c56f2ee3';
-  recipient = 'cef48d2ec4086bd5799b659261948daab02b760d';
+  privateKey = "db11cfa086b92497c8ed5a4cc6edb3a5bfe3a640c43ffb9fc6aa0873c56f2ee3";
+  recipient = "cef48d2ec4086bd5799b659261948daab02b760d";
 } else {
   // User supplies the private key through `--key`
   if (argv.key) {
     privateKey = argv.key;
     console.log(`Your Private Key: ${privateKey} \n`);
   } else {
-    console.log('No private key given! Generating random privatekey.'.green);
+    console.log("No private key given! Generating random privatekey.".green);
     privateKey = zilliqa.util.generatePrivateKey();
-    console.info(`Your Private Key: ${privateKey.toString('hex')}`);
+    console.info(`Your Private Key: ${privateKey.toString("hex")}`);
   }
 
   if (!argv.to) {
-    console.log('To address required');
+    console.log("To address required");
     process.exit(0);
   }
   recipient = argv.to;
 }
 
-let nonceVal = 2;
-if(argv.n) { 
-  nonceVal = argv.n;
-}
-
-
-const address = zilliqa.util.getAddressFromPrivateKey(privateKey);
-
-const node = zilliqa.getNode();
-console.log(`Address: ${address}`);
-
-function callback(err, data) {
-  if (err || data.error) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
-}
-
-/*  MAIN LOGIC  */
-
-console.log('Zilliqa Testing Script');
-console.log(`Connected to ${url}`);
-
-/* Contract specific Parameters */
-
-// the immutable initialisation variables
+const senderAddr = zilliqa.util.getAddressFromPrivateKey(privateKey);
+console.log(senderAddr);
 const msg = {
-  _tag: 'setHello',
-  _amount: '0',
-  _sender: '0x7bb3b0e8a59f3f61d9bff038f4aeb42cae2ecce8',
-  params: [{
-    vname: 'msg',
-    type: 'String',
-    value: 'Morning',
-  }],
+  _tag: "setHello",
+  _amount: "0",
+  _sender: `0x${senderAddr}`,
+  params: [
+    {
+      vname: "msg",
+      type: "String",
+      value: "Morning",
+    },
+  ],
 };
 
-// transaction details
-const txnDetails = {
-  version: 0,
-  nonce: nonceVal,
-  to: recipient,
-  amount: new BN(0),
-  gasPrice: 1,
-  gasLimit: 2000,
-  data: JSON.stringify(msg).replace(/\\"/g, '"'),
-};
 
-// sign the transaction using util methods
-const txn = zilliqa.util.createTransactionJson(privateKey, txnDetails);
-console.log(txn);
+/*
+*   Main Logic
+*/
 
-// send the transaction to the node
-node.createTransaction(txn, callback);
+// Get user's nonce and increment it by one before sending transaction
+getNonceAsync(senderAddr)
+  .then(nonce => {
+    const nonceVal = nonce + 1;
+    console.log(`Payload's Nonce is ${nonceVal}`);
+    const xnDetails = makeTxnDetails(nonceVal);
+    const txn = zilliqa.util.createTransactionJson(privateKey, txnDetails);
+    zilliqa.node.createTransaction(txn, (err, data) => {
+        if (err || data.error) {
+          console.log(err);
+        } else {
+          console.log(data);
+        }
+      });
+  })
+  .catch(err => console.log(err));
